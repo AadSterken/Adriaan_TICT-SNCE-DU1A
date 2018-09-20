aece6d90fe9a742619a83356ee4f48d808dbbfe6
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-20 13:47:17 +0200

    Versie1

---
+++ 3. Input-Output.py
@@ -0,0 +1,7 @@
+uren = float(input('Hoeveel uur heb je gewerkt: '))
+
+uurloon = float(input('Wat verdien je per uur: '))
+
+verdiend = round((uren * uurloon), 2)
+
+print('Je hebt ' + str(uren) + ' gewerkt en ' + str(verdiend) + ' verdiend')

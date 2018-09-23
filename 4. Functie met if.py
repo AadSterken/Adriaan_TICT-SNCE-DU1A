637be6683e0c9a14d27e3543ce5a977e7828e529
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-23 12:38:34 +0200

    Function

---
+++ 4. Functie met if.py
@@ -0,0 +1,28 @@
+old = (input('Enter your old password: '))
+new = (input('Enter a new password: '))
+a = True
+b = False
+x = 0
+
+
+def new_password(n = new, o = old):
+    l=0
+    if n != o:
+        if len(n) > 5:
+            for l in range(len(n)):
+                if x < 1:
+                    s = n[l]
+                    if s.isdigit() == a:
+                        print(a)
+                        exit()
+                    else:
+                        l + 1
+                else:
+                    return
+        else:
+            print(b)
+    else:
+        print(b)
+
+
+new_password(new, old)

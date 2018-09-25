3917b37e71972b880658e24681957049009407bd
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-25 22:28:56 +0200

    Function

---
+++ 4. For, If & strings.py
@@ -0,0 +1,7 @@
+dagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag'
+         'vrijdag', 'zaterdag', 'zondag']
+
+for i in range(0, len(dagen)):
+    a = dagen[i]
+    print(a[0] + a[1])
+    i=i+1

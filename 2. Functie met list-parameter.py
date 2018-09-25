a83b2c46aa782087bc9deecad63eb58e0fa270ae
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-25 21:24:04 +0200

    Function

--- 2. Functie met list-parameter.py
+++ 2. Functie met list-parameter.py
@@ -1,12 +1,12 @@
-getallenlijst = [2, 5, 7, 3, 9, 2]
+getallenlijst = [2, 5, 7, 3, 9, 2, 8]
 totaal = 0
 
 
 def som(g, s):
-    for t in range(1, (len(g))):
-        while t < (len(g) + 1):
-            s = s + g[t]
-        return s
+    for t in range(0, (len(g))):
+        s = s + g[t]
+        t=t+1
+    return s
 
 
 print(som(getallenlijst, totaal))

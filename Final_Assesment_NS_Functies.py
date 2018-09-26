af2a4bd59d5c5024c9c1f5d5b83f2a906d671b69
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-26 22:45:24 +0200

    Function

---
+++ Final_Assesment_NS_Functies.py
@@ -0,0 +1,25 @@
+afstandKM = float(5)
+prijs = float(0)
+leeftijd = float(0)
+weekend = ('ja' == True)
+
+
+def standaardprijs(a, p):
+    p=0
+    if a < 50:
+        p=a*0.80
+    else:
+        p=(a*.60)+15
+    return print(p)
+
+
+def ritprijs(s, l, w, a):
+    for l > 12 & l <
+        for w = True:
+            p1 = s * 0.40
+        else:
+            
+
+
+standaardprijs(afstandKM, prijs)
+ritprijs(standaardprijs(), leeftijd, weekendrit, afstandKM)

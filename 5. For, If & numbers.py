b29588b1cf963222d70d31435da861f6d9ad900a
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-26 17:41:47 +0200

    Function

--- 5. For, If & numbers.py
+++ 5. For, If & numbers.py
@@ -1,6 +1,5 @@
 getallen = [1, 5, 8, 4, 9, 0, 3, 6]
 
-for i in range(0, len(getallen)):
-    if (getallen[i]%2) == 0:
-        print(getallen[i])
-        i=i+1
+for i in getallen:
+    if (i%2) == 0:
+        print(i)

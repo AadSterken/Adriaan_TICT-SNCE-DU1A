45990f9dd7aa4fba5da4069feb8040088980abe1
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-26 17:37:30 +0200

    Function

--- 6. For, If & vowels.py
+++ 6. For, If & vowels.py
@@ -2,12 +2,8 @@ s = "Guido van Rossum heeft programmeertaal Python bedacht."
 
 
 def klinkers(s):
-    k = ""
-    for i in range(len(s)):
-        if (((s[i] == 'a') or (s[i] == 'e')) or
-        (s[i] == 'i')) or ((s[i] == 'o') or (s[i] == 'u')):
-            k = k + s[i]
-            i = i + 1
-    print(k)
+    for i in s:
+        if i in "aeuio":
+            print(i)
 
 klinkers(s)

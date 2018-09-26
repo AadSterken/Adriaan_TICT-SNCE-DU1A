e8aeb37ce494c61e2c0335b86dc4d73e03205259
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-09-26 17:53:20 +0200

    Function

--- 5. Functie met list-parameter en for-loop.py
+++ 5. Functie met list-parameter en for-loop.py
@@ -3,9 +3,9 @@ grondgetallen = [3, 9, -5, 2, -5]
 
 def kwadratensom(g):
     s = 0
-    for i in range(len(g)):
-        if g[i] > 0:
-            k = g[i]**2
+    for i in g:
+        if i > 0:
+            k = i**2
             s = s + k
             i = i + 1
     print(s)

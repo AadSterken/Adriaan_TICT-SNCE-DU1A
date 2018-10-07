8a4585d3659757031f5d0342f8b6add2c0447576
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-10-07 20:18:08 +0200

    Les 7

--- 4. Files schrijven.py
+++ 4. Files schrijven.py
@@ -19,7 +19,8 @@ while True:
 
 
     def invoer():
-        invoer = input('Vul hier je tijd en je naam in in het volgende format: 11:15:48 Jan Janssen \n')
+        invoer = input('Vul hier je tijd en je naam in in het volgende format: 11:15:48 Jan Janssen.\n '
+                       'Type \'stop\' om te stoppen \n')
         # invoer = invoer.split('')
         # print(invoer)
         return invoer
@@ -30,9 +31,6 @@ while True:
             invoer = invoer.split(' ')
             naam = invoer[1] + ' ' + invoer[2]
             tijd = invoer[0]
-            print(naam)
-            print(tijd)
-            print(datum)
             file.writelines('{0:8} {1:8} {2:8}'.format(datum, tijd, naam) + '\n')
         else:
             exit()

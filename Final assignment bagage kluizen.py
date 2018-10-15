997ded428a4346a83301ec259e1e11ee23dcbab7
AadSterken <adriaan.sterkenbrg@student.hu.nl>
2018-10-15 21:52:04 +0200

    Les 7

--- Final assignment bagage kluizen.py
+++ Final assignment bagage kluizen.py
@@ -1,54 +1,98 @@
+kluizen = "kluizen.txt"
+
+
+def toon_aantal_kluizen_vrij(kluizen):
+    file = open(kluizen, "r")
+    counter = 0
+    for line in file.readlines():
+        split = line.split(";")
+        wachtwoord = split[1].rstrip()
+        if wachtwoord == "":
+            counter += 1
+    file.close()
+    return counter
+
+
+def nieuwe_kluis(kluizen):
+    fileRead = open(kluizen, "r")
+    lines = fileRead.readlines()
+    for line in lines:
+        split = line.split(";")
+        kluis = split[0].rstrip ()
+        wachtwoord = split[1].rstrip()
+        if wachtwoord == "":
+            while True:
+                invoer = input('Voer uw wachtwoord in: ')
+                if len(invoer) >= 4:
+                    lines[int(kluis) - 1] = kluis + ";" + str(invoer) + "\n"
+                    fileWrite = open(kluizen, "w")
+                    fileWrite.writelines(lines)
+                    fileWrite.close()
+                    print("U kluis is:" + str(kluis))
+                    break
+                else:
+                    print("Uw wachtwoord is te kort")
+        fileRead.close()
+
+
+def kluis_openen(kluizen):
+    file = open(kluizen, 'r')
+    authenticated = False
+    invoer_kluis = input('Voer uw kluisnummer in: ')
+    invoer_ww = input('Voer uw wachtwoord in: ')
+    for line in file.readlines():
+        split = line.split(";")
+        kluis = split[0].rstrip()
+        wachtwoord = split[1].rstrip()
+        if (invoer_kluis == str(kluis)) and (invoer_ww == str(wachtwoord)):
+            print("Kluis:" + str(kluis) + " nu open!")
+            authenticated = True
+    if authenticated == False:
+        print('Uw kluisnummer en wachtwoord komen niet overeen.')
+    file.close()
+
+
+def kluis_teruggeven(kluizen):
+    released = False
+    fileRead = open(kluizen, 'r')
+    lines = fileRead.readlines()
+    invoer_kluis = input('Voer uw kluisnummer in: ')
+    invoer_ww = input('Voer uw wachtwoord in: ')
+    for line in lines:
+        split = line.split(";")
+        kluis = split[0].rstrip ()
+        wachtwoord = split[1].rstrip()
+        if (invoer_kluis == str(kluis)) and (invoer_ww == str(wachtwoord)):
+            lines[int(kluis) - 1] = kluis + ";" + "" + "\n"
+            fileWrite = open(kluizen, "w")
+            fileWrite.writelines(lines)
+            fileWrite.close()
+            print("U heeft kluis " + str(kluis) + " ingeleverd.")
+            released = True
+    if released == False:
+        print("Kluis of wachtwoord incorrect")
+    fileRead.close()
 
 
 
-def kluisreg():
 
-    regels = 0
+while True:
+    print('1: Ik wil weten hoeveel kluizen nog vrij zijn')
+    print('2: Ik wil een nieuwe kluis')
+    print('3: Ik wil even iets uit mijn kluis halen')
+    print('4: Ik geef mijn kluis terug')
+    print('5. Ik wil stoppen')
+    invoer = input('')
+    if invoer == str(1):
+        print('er zijn nog ' + str(toon_aantal_kluizen_vrij(kluizen)) + ' kluizen vrij')
+    elif invoer == str(2):
+        nieuwe_kluis(kluizen)
+    elif invoer == str(3):
+        kluis_openen(kluizen)
+    elif invoer == str(4):
+        kluis_teruggeven(kluizen)
+    elif invoer == str(5):
+        break
+    else:
+        print('Uw heeft geen correcte invoer gegeven')
 
-    while True:
-
-
-
-        print('1: Ik wil weten hoeveel kluizen nog vrij zijn')
-        print('2: Ik wil een nieuwe kluis')
-        print('3: Ik wil even iets uit mijn kluis halen')
-        print('4: Ik geef mijn kluis terug')
-        invoer = int(input('Voer het gewenste nummer in: '))
-
-        kluizen = open(r"C:\Users\Adriaan HU\PycharmProjects\Adriaan_TICT-SNCE-DU1A\kluizen.txt", 'w')
-        regels = kluizen.readlines()
-
-        # for line in kluizen.readlines():
-        #     regels += 1
-
-        if invoer == 1:
-            print('Er zijn nog ' + str(10 - regels) + ' beschikbaar')
-        elif invoer == 2:
-            kluisnummer = input('voer uw kluisnummer in: ')
-            code = input('voer uw code in: ')
-            kluizen.writelines(code + '; ' + kluisnummer)
-
-# ik zou het wegschrijven in het tekstbestand in een functie kunnen zetten
-
-        elif invoer == 3:
-            print(invoer)
-            # kluisnummer = input('voer uw kluisnummer in: ')
-            # code = input('voer uw code in: ')
-
-# controleren of het overeenkomt
-# zoniet dan foutmelding
-# indien correct aangeven dat kluis oven kan
-
-        elif invoer == 4:
-            print(invoer)
-            # kluisnummer = input('voer uw kluisnummer in: ')
-            # code = input('voer uw code in: ')
-
-# controleren of het overeenkomt
-# zoniet dan foutmelding
-# indien correct aangeven dat kluis oven kan
-        elif (invoer <  1) or (invoer > 4):
-            input('U heeft een fout nummer ingevoerd, type OK om weer terug te komen in het keuzemenu.')
-
-
-kluisreg()
